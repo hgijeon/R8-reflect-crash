fec905f06ad102fdee274e66bfcd4537cb230192
hgijeon <hgijeon@gmail.com>
2021-11-13 18:04:24 +0900

    crashes when using reflection with r8

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlin.reflect.full.memberProperties
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val testInstance = TestClass()
+        testInstance.testProperty // prevent optimizing out
+        testInstance::class.memberProperties // CRASH
     }
+
+    enum class TestEnum { TEST_ENTRY }
+}
+
+class TestClass {
+    val testProperty = MainActivity.TestEnum.TEST_ENTRY
 }
